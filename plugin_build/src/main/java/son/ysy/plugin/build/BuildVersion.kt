9d0674ea85e67c89569e5d9f3bad3cca0e47a9db
YCX-T <974577817@qq.com>
2020-08-03 20:27:31 +0800

    build: rebuild &publish 1.0.1

---
+++ plugin_build/src/main/java/son/ysy/plugin/build/BuildVersion.kt
@@ -0,0 +1,13 @@
+package son.ysy.plugin.build
+
+sealed class BuildVersion<T>(val newest: T) {
+    fun custom(value: T) = value
+
+    object CompileSdk : BuildVersion<Int>(30)
+
+    object MinSdk : BuildVersion<Int>(21)
+
+    object TargetSdk : BuildVersion<Int>(29)
+
+    object BuildTools : BuildVersion<String>("30.0.1")
+}
